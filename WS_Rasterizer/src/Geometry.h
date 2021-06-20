c30c24ecc7bb3bc95bd889f604acb69f109fe1c7
Mohido <m.ghaithalmahdawi@gmail.com>
2021-06-20 05:11:34 +0200

    Simple Interpolation Ratio + Area to Triangles
    Added the basics of the interpolation ratio: Now we can get the Barcyntric
    interpolation from points inside the screen_image. Next will be
    implementing the Z-Buffering algorithm and then the Vertices' Attributes
    interpolation.
    
    Now you can get the traingle area, which is formed when the triangle is
    created by using the determinants of the vectors.

--- WS_Rasterizer/src/Geometry.h
+++ WS_Rasterizer/src/Geometry.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef Geometry
 #define Geometry
+#include <vector>
 
 
 namespace geometry {
@@ -16,7 +17,7 @@ namespace geometry {
 	struct Vector_2D {
 		T x, y;
 		Vector_2D(T x, T y) : x(x), y(y) {}
-
+		Vector_2D() : x(0), y(0) {}
 
 		/*
 		* Cross Product Function for 2 2d vectors. Returns the scaler of the Z axis.
@@ -28,9 +29,8 @@ namespace geometry {
 			return this->x * v2.y - this->y * v2.x;		 // determinant (Length of the new axis)
 		}
 
-		/*
-		* Minus overriding
-		*/
+		T lengthSqr() const { return this->x * this->x + this->y * this->y; }
+		/* Minus overriding*/
 		Vector_2D<T> operator-(Vector_2D<T> v2) {	return Vector_2D<T>(this->x - v2.x, this->y - v2.y); }
 
 	};
@@ -41,24 +41,75 @@ namespace geometry {
 		Vector_3D(T x, T y, T z) : x(x), y(y), z(z) {}
 		Vector_3D() { this->x = 0; this->y = 0; this->z = 0; }
 		T lengthSqr() const { return this->x*this->x + this->y*this->y + this->z*this->z; }
+		Vector_3D<T> operator-(Vector_3D<T> v2) {	 return Vector_3D<T>(this->x - v2.x, this->y - v2.y, this->z - v2.z); }
 	};
 
 
 
 	// Geometric Shapes - Polygons Area
 	template<class T>
-	struct Triangle_3D {
+	class Triangle_3D {
 		Vector_3D<T> vertex_1, vertex_2, vertex_3;
+		float area;
+	public:
 		Triangle_3D(Vector_3D<T> v1, Vector_3D<T> v2, Vector_3D<T> v3)
 			: vertex_1(v1), vertex_2(v2), vertex_3(v3)
-		{}
+		{
+			// This area needs to be implemented when matrix is implemented and the determinant of matrix is implemented
+			// Or the crossProduct of the Vector_3D is implemented. we can simply do as we did in teh Triangle 2d
+			this->area = 0;
+		}
 
 		Triangle_3D() {
 			this->vertex_1 = { 0 };
 			this->vertex_2 = { 0 };
 			this->vertex_3 = { 0 };
 		}
+
+		/*Getters Area*/
+		float getArea() { return this->area; }
+		std::vector< Vector_3D<T> > getVertices() { return { vertex_1, vertex_2, vertex_3 };}
 	};
+
+
+
+	template<class T>
+	class Triangle_2D {
+		Vector_2D<T> vertex_1, vertex_2, vertex_3;
+		float area;
+	public:
+		Triangle_2D(Vector_2D<T> v1, Vector_2D<T> v2, Vector_2D<T> v3)
+			: vertex_1(v1), vertex_2(v2), vertex_3(v3)
+		{
+			// according to the common sense, the area of a triangle is the 1/2 * parallalgram area that represents the triangle
+			// So from 2 sides we can get the area of the triangle.
+			Vector_2D<T> side1 = v2 - v1;
+			Vector_2D<T> side2 = v3 - v1;
+			// Their detirmenant is the area of the parallelgram, (CrossProduct length)
+			this->area = std::abs(side2.crossProduct(side1)) / 2;
+		}
+
+		Triangle_2D() {
+			this->vertex_1 = { 0 };
+			this->vertex_2 = { 0 };
+			this->vertex_3 = { 0 };
+			this->area = 0;
+		}
+
+		/*Checks if the given point is inside the triangle or not by using the edge function*/
+		bool pointInside(Vector_2D<T> point) {
+			return (vertex_2 - vertex_1).crossProduct(point - vertex_1) >= 0
+				&& (vertex_3 - vertex_2).crossProduct((point - vertex_2)) >= 0
+				&& (vertex_1 - vertex_3).crossProduct((point - vertex_3)) >= 0;
+		}
+
+		float getArea() { return this->area; }
+		std::vector< Vector_2D<T> > getVertices() { return { vertex_1, vertex_2, vertex_3 }; }
+	};
+
+
+
+
 
 }
 
--- WS_Rasterizer/src/main.cpp
+++ WS_Rasterizer/src/main.cpp
@@ -1,14 +1,12 @@
 #include <windows.h>    // _WINDOWS_ 
 #include <string>
-#include <algorithm>
 #include <random>
-#include "Geometry.h"
+#include "geometry.h"
 #include "renderer.h"
 
 #define SEC_TO_NANOSEC 1000000000LL
 #define SEC_TO_MILLISEC 1000LL
 #define NANOSECONDS_PER_FRAME (SEC_TO_NANOSEC/60LL)
-#define STARS_COUNT 10000 // you can go crazy here
 
 
 
@@ -38,6 +36,7 @@ int WINAPI WinMain(HINSTANCE processId, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     long long lastTime = getCurrentTime_nanoseconds();
     MSG msg = { 0 };
     bool running = true;
+
     while (running) {
         /*Process new events*/ // Note: Peekmessage returns false if there is no new message
         while (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) { // peek does not halt the program, unlike getMessage(); 
@@ -54,35 +53,71 @@ int WINAPI WinMain(HINSTANCE processId, HINSTANCE hPrevInstance, PSTR lpCmdLine,
             //..
             /*Rendering logic*/
             renderer::clearPixels((char*)pixelsArray, displayWidth, displayHeight, bytesPerPixel);                  // clearing hte pixels sheet
-
+            renderer::Color col = { 255,255,255,0 };
 
             /*Drawing the Triangles*/
             geometry::Vector_3D<float> v1(100.0, -100.0, 1.0);
             geometry::Vector_3D<float> v2(-100.0, -100.0, 1.0);
             geometry::Vector_3D<float> v3(0.0, 100.0, 1.0);
-            //geometry::Triangle_3D<float> tri(v1,v2,v3);
+            geometry::Triangle_3D<float> tri(v1,v2,v3);
 
             //projecting the 3 points..
+            /// TODO: Check for division by 0, 
+            /// TODO: Change the implementation to be directly with triangles instead of defining 3 vector variables
             geometry::Vector_2D<int> sv1((v1.x / v1.z) + displayWidth / 2, (v1.y / -v1.z) + displayHeight / 2);
             geometry::Vector_2D<int> sv2((v2.x / v2.z) + displayWidth / 2, (v2.y / -v2.z) + displayHeight / 2);
             geometry::Vector_2D<int> sv3((v3.x / v3.z) + displayWidth / 2, (v3.y / -v3.z) + displayHeight / 2);
+            geometry::Triangle_2D<int> tri_screen(sv1,sv2,sv3);
 
-            renderer::Color col = { 255,255,255,0 };
-            
+            /*
+            * The triangle looks as follows:
+                       sv3
+                       / \
+                      /   \
+                     /     \
+                 sv2 ------ sv1
+            */
+
+
             /*Going through the boxing boundries of the triangle*/
-            for (int x = min(sv1.x, min(sv2.x, sv3.x)); x <= max(sv1.x, max(sv2.x, sv3.x)); x++) {
-                for (int y = min(sv1.y, min(sv2.y, sv3.y)); y <= max(sv1.y, max(sv2.y, sv3.y)); y++) {
+            
+            for (int y = min(sv1.y, min(sv2.y, sv3.y)); y <= max(sv1.y, max(sv2.y, sv3.y)); y++) {
+                for (int x = min(sv1.x, min(sv2.x, sv3.x)); x <= max(sv1.x, max(sv2.x, sv3.x)); x++) {
                     geometry::Vector_2D<int> point(x,y);
-                    // Using the EdgeFunction Algorithm to quickly check if a point is inside a triangle or not
-                    bool isInside = (sv2 - sv1).crossProduct(point - sv1) >= 0
-                                 && (sv3 - sv2).crossProduct((point - sv2)) >= 0
-                                 && (sv1 - sv3).crossProduct((point - sv3)) >= 0;
 
-                    if(isInside)
+                    bool isInside = tri_screen.pointInside(point);  // Using the EdgeFunction Algorithm to quickly check if a point is inside a triangle or not
+
+                    if (isInside) {
+                       /* For Debugging
+                       std::wstring ms = L"The T_2:\n(" +
+                            std::to_wstring((point - sv2).x) + L"," +  std::to_wstring((point - sv2).y) + L") \n" +
+                            L" (" + std::to_wstring((point - sv3).x) +  L"," + std::to_wstring((point - sv3).y) + L") \n" +
+                            L"And point is: " + std::to_wstring(point.x) + L"," + std::to_wstring(point.y) + L") \n";
+                            
+                            */
+
+
+                        /*Getting the Ratios of the point (Barcyntric Interpolation)*/
+                        float area_1 = abs((point - sv2).crossProduct(point - sv3)) / 2; // Parallelgram Area / 2... 
+                        float ratio_1 = area_1 / tri_screen.getArea();
+
+                        float area_2 = abs((sv1 - point).crossProduct(sv3 - point)) / 2;
+                        float ratio_2 = area_2 / tri_screen.getArea();
+
+                        float ratio_3 = 1 - (ratio_1 + ratio_2); // this will have some errors mostly because of close operations on floats
+
+                      /*  printMessage(std::to_wstring(ratio_2));
+                        printMessage(std::to_wstring(ratio_3));
+                        printMessage(std::to_wstring(ratio_1));
+                        printMessage(L"\n");*/
+
+
+                        /*Rendering the point*/
                         renderer::renderPixel((char*)pixelsArray, displayWidth, displayHeight, bytesPerPixel, x, y, col, 1);
+                    }
+                        
                 }
             }
-            
 
             /*Rendering the canvas to the screen*/
             renderer::renderCanvas(windowId, pixelsArray, displayWidth, displayHeight, bytesPerPixel);              // re-initialize the canvas
@@ -97,7 +132,6 @@ int WINAPI WinMain(HINSTANCE processId, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     //delete[] stars; // tried to render 1000000 stars .. and it works super fine lol!!
     return 0;
 }
-
 
 
 
@@ -117,11 +151,6 @@ void printMessage(const std::wstring& msg) {
 
 
 
-
-
-
-
-
 /*
 * Explanation: A high resolution time query.
 * Returns: The current high-resolution system time in nanoseconds.
@@ -132,12 +161,6 @@ long long getCurrentTime_nanoseconds() {
     QueryPerformanceFrequency(&ticksPerSec);                            //  ticks per second
     return (SEC_TO_NANOSEC * ticks.QuadPart) / ticksPerSec.QuadPart;    // return the elapsed time in nanoseconds
 }
-
-
-
-
-
-
 
 
 
@@ -190,8 +213,6 @@ HWND initWindow(HINSTANCE processId, const unsigned int& displayWidth, const uns
     );
     return windowId;
 }
-
-
 
 
 
